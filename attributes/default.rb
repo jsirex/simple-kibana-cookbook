9acc939c9cd546cfc8578c0180b0515814679747
Yauhen Artsiukhou <jsirex@gmail.com>
2015-06-06 22:55:15 +0300

    Bump kibana default version to 4.0.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default['kibana']['download_url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-linux-x64.tar.gz'
-default['kibana']['checksum'] = '8f1de21f52803793dafeefe54f5b8933c2d3887ecbf69da6835430586b25cf2e'
-default['kibana']['version'] = '4.0.0'
-
+default['kibana']['download_url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.2-linux-x64.tar.gz'
+default['kibana']['checksum'] = '4cc36e5c6ca7c495667319df75feda1facb7c43a3d9686841f07a2522adec294'
+default['kibana']['version'] = '4.0.2'
+f
 default['kibana']['user'] = 'kibana'
 default['kibana']['group'] = 'kibana'
 default['kibana']['dir'] = '/opt'

6011f98e49bb73a4593063831c44776c1c153a51
Virender Khatri <vir.khatri@gmail.com>
2015-06-06 20:17:27 +0530

    issue #5, use node kibana version attribute in download url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['kibana']['download_url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-linux-x64.tar.gz'
-default['kibana']['checksum'] = '8f1de21f52803793dafeefe54f5b8933c2d3887ecbf69da6835430586b25cf2e'
 default['kibana']['version'] = '4.0.0'
+default['kibana']['download_url'] = "https://download.elasticsearch.org/kibana/kibana/kibana-#{node['kibana']['version']}-linux-x64.tar.gz"
+default['kibana']['checksum'] = '8f1de21f52803793dafeefe54f5b8933c2d3887ecbf69da6835430586b25cf2e'
 
 default['kibana']['user'] = 'kibana'
 default['kibana']['group'] = 'kibana'

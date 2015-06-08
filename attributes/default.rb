eb5c8df85c36ebb44915d748845cdf6de19b7369
Julien Pervillé <julien.perville@perfect-memory.com>
2015-06-08 16:13:38 +0630

    Typo fix.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['kibana']['download_url'] = 'https://download.elasticsearch.org/kibana/kibana/kibana-4.0.2-linux-x64.tar.gz'
 default['kibana']['checksum'] = '4cc36e5c6ca7c495667319df75feda1facb7c43a3d9686841f07a2522adec294'
 default['kibana']['version'] = '4.0.2'
-f
+
 default['kibana']['user'] = 'kibana'
 default['kibana']['group'] = 'kibana'
 default['kibana']['dir'] = '/opt'

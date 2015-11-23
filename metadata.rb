f4964ece26e1679d2cce1913d09ff7be85aa7707
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-11-23 13:16:59 +0300

    Bump new version: v0.4.3

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs Kibana ~> 4.0. No less, no more'
 issues_url       'https://github.com/jsirex/simple-kibana-cookbook/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/jsirex/simple-kibana-cookbook' if respond_to?(:source_url)
 
-version          '0.4.2'
+version          '0.4.3'
 
 depends 'ark'
 depends 'runit'

43708bca85ec7a1997d4679d911a4af845acd614
Fabien Jakimowicz <fabien@jakimowicz.com>
2015-12-25 23:57:12 +0100

    fixing 4.3.1 sha256 checksum and adding 4.1.4 version

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,8 +3,12 @@ SimpleKibanaVersions  = {
     checksum:     'f2cb5389ad0acfbc4006f739d75d5ede541483d1fa6be728bbf547a9d7ddeb4a',
     download_url: 'https://download.elasticsearch.org/kibana/kibana/kibana-4.1.3-linux-x64.tar.gz'
   },
+  '4.1.4' => {
+    checksum:     '2d11c63268f49b95726bfa0f3c09cf898ea67b65da3599989d17a27095987b0d',
+    download_url: 'https://download.elastic.co/kibana/kibana/kibana-4.1.4-linux-x64.tar.gz'
+  },
   '4.3.1' => {
-    checksum:     '115ba22882df75eb5f07330b7ad8781a57569b00',
+    checksum:     'c6a91921a0055714fd24fb94a70b7057f43492da6bd8c4f2f1acbf0964bf09b9',
     download_url: 'https://download.elastic.co/kibana/kibana/kibana-4.3.1-linux-x64.tar.gz'
   }
 }

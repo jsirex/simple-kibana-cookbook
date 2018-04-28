e80393dec2c249b170138b87cb0936790646e5cd
Yauhen Artsiukhou <jsirex@gmail.com>
2018-04-28 09:29:41 +0300

    Set kibana 4.6.6 as default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['kibana']['download_url'] = 'https://download.elastic.co/kibana/kibana/kibana-4.5.0-linux-x64.tar.gz'
-default['kibana']['checksum'] = 'fa3f675febb34c0f676f8a64537967959eb95d2f5a81bc6da17aa5c98b9c76ef'
-default['kibana']['version'] = '4.5.0'
+default['kibana']['download_url'] = 'https://download.elastic.co/kibana/kibana/kibana-4.6.6-linux-x86_64.tar.gz'
+default['kibana']['checksum'] = '8c3c1808349ac9645836cae35c5570a53091b44b2212c0826907efae1a865d17'
+default['kibana']['version'] = '4.6.6'
 
 default['kibana']['user'] = 'kibana'
 default['kibana']['group'] = 'kibana'
